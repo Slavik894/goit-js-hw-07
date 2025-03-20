508214ed2c8cbc1720008716d9a2ff1132c62c64
Slavik <slavik1058@gmail.com>
2025-03-21 00:24:51 +0100

    complete first task

--- js/task-1.js
+++ js/task-1.js
@@ -1,26 +1,28 @@
 const categories = document.querySelectorAll(".item");
 const titles = document.querySelectorAll("h2");
 let counter = 0;
-let itemTitle;
+
 
 // counts categories
 function countCategories(categories){
-    categories.forEach((category) =>
+    categories.forEach(category =>
     counter++);
    return `Number of categories: ${counter}`;
 };
 console.log(countCategories(categories));
 
 
-// shows titles but doesn't work yet
-function showTitles(titles){
-    titles.forEach((title) => {
-        itemTitle = title.textContent;
-    return `The category is ${itemTitle}`
-    });
-    
-}
-console.log(showTitles(titles));
+// shows titles 
+function showTitles(titlesList){
+    const result = titlesList.forEach(title => { console.log(`The category is ${title.textContent}`);
+    const parent = title.parentElement;
+    const ul = parent.querySelector('ul');
+    const itemLength = ul.querySelectorAll('li').length
+    console.log(`The amount is: ${itemLength}`);      
+});  
+return result;
+};
+showTitles(titles);
 
 
 
