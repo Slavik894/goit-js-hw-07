96918096cdf76fe663d9162b5aa586130a2bd0b9
Slavik <slavik1058@gmail.com>
2025-03-21 23:36:34 +0100

    styles for task 1

--- js/task-1.js
+++ js/task-1.js
@@ -5,8 +5,13 @@ let counter = 0;
 
 // counts categories
 function countCategories(categories){
-    categories.forEach(category =>
-    counter++);
+    categories.forEach(category =>{
+        category.style.listStyle = "none";
+        category.style.background = "#f6f6fe";
+        category.style.padding = "16px";
+
+        category.style.borderRadius = "8px";
+    counter++});  
    return `Number of categories: ${counter}`;
 };
 console.log(countCategories(categories));
@@ -14,16 +19,35 @@ console.log(countCategories(categories));
 
 // shows titles 
 function showTitles(titlesList){
-    const result = titlesList.forEach(title => { console.log(`The category is ${title.textContent}`);
+    const result = titlesList.forEach(title => { console.log(`Category: ${title.textContent}`);
+    title.style.margin = "0px";
+    title.style.marginBottom = "16px";
     const parent = title.parentElement;
     const ul = parent.querySelector('ul');
-    const itemLength = ul.querySelectorAll('li').length
-    console.log(`The amount is: ${itemLength}`);      
+    ul.style.listStyleType = "none";
+    ul.style.padding = "0px"
+    // const itemLength = ul.querySelectorAll('li').length;
+    const items = ul.querySelectorAll('li');
+    items.forEach(item =>{
+        item.style.border = "1px solid #808080";
+        item.style.borderRadius = " 4px";
+        item.style.marginBottom = "8px";
+        item.style.paddingLeft = "16px";
+        item.style.paddingTop = "8px";
+        item.style.paddingBottom = "8px";
+
+    });
+    const itemLength = items.length;
+    console.log(`Elements: ${itemLength}`);      
 });  
 return result;
 };
 showTitles(titles);
 
+const categoriesUl = document.querySelector("#categories");
+categoriesUl.style.display = "flex";
+categoriesUl.style.flexDirection = "column"
+categoriesUl.style.gap = "24px";
 
 
 
